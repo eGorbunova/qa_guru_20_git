695f4785800271000457efd458db0f0746154687
Ekaterina Gorbunova <gorbunova.kate29@gmail.com>
2023-06-02 18:00:57 +0300

    var added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello qa guru!!!!");
+        String message = "Hello qa guru!!!!";
+        System.out.print(message);
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
